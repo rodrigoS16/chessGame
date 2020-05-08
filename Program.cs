2ec186b4e212e4f52c8180ce1912ad201d5b1ce1
Rodrigo Castilho <rodrigoS16unicesumar@gmail.com>
2020-05-08 18:23:12 -0300

    Projeto criado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jogo_xadres_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
