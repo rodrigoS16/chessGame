d7102e7809d37decde8d585019921d9b4882b29a
Rodrigo Castilho <rodrigoS16unicesumar@gmail.com>
2020-05-08 18:39:48 -0300

    Class Position created

---
+++ Tabuleiro/Position.cs
@@ -0,0 +1,14 @@
+﻿namespace jogo_xadres_console.Tabuleiro
+{
+    class Position
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Position(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+    }
+}
